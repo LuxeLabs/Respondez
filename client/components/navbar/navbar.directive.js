c90de04e55aa663383b103175a2abdb6b26b71b6
Addison Shaw <addison@xlab.me>
2016-02-02 09:42:54 -0600

    Restarting with a new file structure and technologies.

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('weddingRsvpApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
