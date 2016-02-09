d8161c1895a05570b0dc594ee43798f59091aba5
Addison Shaw <addison@xlab.me>
2016-02-09 03:17:01 -0600

    Formatting

--- controllers/rsvp.js
+++ controllers/rsvp.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
+var express    = require('express');
+var app        = express();
 var connection = null
-var mongoose = require ('mongoose');
+var mongoose   = require ('mongoose');
 
 module.exports = function (conn) {
    connection = conn;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
-var app = require('./controllers/app');
+var app  = require('./controllers/app');
 
 http.createServer(app).listen(port, function () {
    console.log('Listening on ' + port);
