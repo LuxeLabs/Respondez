e0293c9b14caccd1f29e1d1e31bb8606c7baf17d
Addison Shaw <addison@xlab.me>
2016-03-01 12:43:37 -0600

    Converted to an express app. Got rid of unecessary files.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var http = require('http');
-var port = process.env.PORT || 1337;
-var app  = require('./controllers/app');
+var express    = require('express');
+var path       = require('path');
+var bodyParser = require('body-parser');
+var app        = express();
+var favicon    = require('serve-favicon');
+var port       = process.env.PORT || 1337;
 
 if (!process.env.UPDATE_PHONE) throw Error('Missing: process.env.UPDATE_PHONE');
 if (!process.env.UPDATE_EMAIL) throw Error('Missing: process.env.UPDATE_EMAIL');
@@ -10,6 +13,32 @@ if (!process.env.TWILIO_TOKEN) throw Error('Missing: process.env.TWILIO_TOKEN');
 if (!process.env.SENDGRID_KEY) throw Error('Missing: process.env.SENDGRID_KEY');
 if (!process.env.MONGO_CONNECTION) throw Error('Missing: process.env.MONGO_CONNECTION');
 
-http.createServer(app).listen(port, function () {
-   console.log('Listening on ' + port);
+// Jade template setup.
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
+app.use(express.query());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('connect-assets')());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import controllers.
+app.use(require('./controllers/rsvp')());
+
+// Use basic routes.
+app.get('/', function (req, res, next) {
+   res.render('index');
+});
+
+app.get('/success', function (req, res, next) {
+   res.render('success')
+});
+
+// Start app
+app.listen(port, function () {
+  console.log('Listening on ' + port);
 });
